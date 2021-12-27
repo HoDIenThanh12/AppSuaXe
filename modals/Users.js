e7c1f0e4955cc075a290211faf3491a1fa5edf00
diencong <cong@bacoor.co>
2021-12-28 06:39:12 +0700

    xcvxcvxcv

--- modals/Users.js
+++ modals/Users.js
@@ -1,8 +1,22 @@
-import React from 'react';
-class Users { 
-  static _user= Users.getInStance() || null;
-  static getInStance() {
-    if (this._user === null) { 
+import React, { useState, useEffect } from "react";
+import firestore from '@react-native-firebase/firestore';
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+import store from 'react-native-simple-store';
+class Users {
+  static _user = Users.getInStance() || null;
+  id = "";
+  sdt = "";
+  name = "";
+  pass = "";
+  image = "";
+  address = "";
+  luotXem = 0;
+  checkWorker = 0;
+  x = 0;
+  y = 0;
+  addressNow = '';
+  static getInStance () {
+    if (this._user === null) {
       this._user = new Users();
     }
     return this._user;
@@ -13,10 +27,59 @@ class Users {
       this._user = new Users();
     }
   }
-  name = 'cong';
-  sdt = '0392225405';
-   setName(name) {
+  async regitster (name, std, pass, address, checkWorker = 0) {
+
+  }
+
+  setCheckWorker (checkWorker) {
+    this.checkWorker = checkWorker;
+  }
+  getCheckWorker () { return this.checkWorker }
+
+  setName (name) {
     this.name = name;
   }
+  getName () { return this.name }
+
+  setSDT (sdt) {
+    this.sdt = sdt
+  }
+  getSDT () { return this.sdt }
+
+  setPass (pass) {
+    this.pass = pass
+  }
+  getPass () { return this.pass }
+
+  setImg (img) {
+    this.image = img
+  }
+  getImg () { return this.image }
+
+  setAddress (address) {
+    this.address = address
+  }
+  getAddress () { return this.address }
+
+  setAddressNow (addressNow) {
+    this.addressNow = addressNow
+  }
+  getAddressNow () { return this.addressNow }
+
+  setX (x) {
+    this.x = x
+  }
+  getX () { return this.x }
+
+  setY (y) {
+    this.y = y
+  }
+  getY () { return this.y }
+
+  setLuotXem (lx) {
+    this.luotXem = lx
+  }
+  getLuotXem () { return this.luotXem }
+
 }
 export default Users;
